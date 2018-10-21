dc99cc48a0e13b1e48dbbbc33bb1cb43776c289d
PimQuatro <43797653+PimQuatro@users.noreply.github.com>
2018-10-20 23:13:51 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,7 @@ const btnLogar = document.querySelector('#logar');
 const URL = '';
 
 validaCampos(form);
-form.addEventListener("input", (e) =>{
-    if (form.email.value && String(form.password.value).length >= 8) {
-    btnLogar.removeAttribute('disabled');
-}
-else {
-    addAttribute(btnLogar, 'disabled');
-}
-});
+
 
 btnLogar.addEventListener('click', e => {
     e.preventDefault();
