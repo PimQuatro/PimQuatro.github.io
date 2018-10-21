52a9346bae7bfb9d4d63fda8e4b168fc1a3b60cf
PimQuatro <43797653+PimQuatro@users.noreply.github.com>
2018-10-20 22:02:09 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,20 @@
 const form = document.querySelector('form');
 const btnLogar = document.querySelector('#logar');
-const URL = 'url/da/api';
+const URL = '';
 
-validaLogin(form);
+validaCampos(form);
 
 btnLogar.addEventListener('click', e => {
     e.preventDefault();
 
-    if (!btnLogar.classList.contains('disabled')) {
+    if (!btnLogar.hasAttribute('disabled')) {
+     
+        getUser(URL, form);
 
-        const login = {
-            email: form.email.value,
-            senha: form.password.value,
-        };
-
-        if(login.email === 'teste@teste.com' && login.senha === 'teste@teste') {
-            window.location = "https://pimquatro.github.io/App/";
-        }else {
-            alert('Email ou senha Inválidos');
-        }
-
-        // const header = {
-        //     method: "POST",
-        //     body: login
-        // };
-
-        // fetch(URL, header)
-        //     .then(response => response.json()) // Espero que venha um hash como token
-        //     .then(data => {
-        //         if (data.erro) {
-        //             alert('Usuario ou Senha Inválidos')
-        //         } else {
-        //             localStorage.setItem('token', data.token);
-        //         }
-        //     })
-        //     .catch()
-
-        // // console.log(localStorage.getItem('token'))
+        // console.log(localStorage.getItem('token'))
 
     } else {
 
     }
 });
+
