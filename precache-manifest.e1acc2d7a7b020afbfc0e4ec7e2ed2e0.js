183b429c55e7d86f7de614fcd5966781c5414461
PimQuatro <43797653+PimQuatro@users.noreply.github.com>
2018-12-09 19:30:10 -0200

    Add files via upload

---
+++ precache-manifest.e1acc2d7a7b020afbfc0e4ec7e2ed2e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b219f91fb228d54fc28c",
+    "url": "/static/css/main.43b4f2a9.chunk.css"
+  },
+  {
+    "revision": "b219f91fb228d54fc28c",
+    "url": "/static/js/main.b219f91f.chunk.js"
+  },
+  {
+    "revision": "118af15335b5aa21dee3",
+    "url": "/static/js/1.118af153.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "827a75464ba57bc396861963c6dccd62",
+    "url": "/static/media/support.827a7546.svg"
+  },
+  {
+    "revision": "3f21f1cf6291fa919affa4b5674432d9",
+    "url": "/index.html"
+  }
+];
